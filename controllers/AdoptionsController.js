11a42aca0a3a48537fabab50262ecb50e3c8f21c
Alister Fenix and Clem Freeman <a.fenix@icloud.com>
2015-02-24 15:47:08 -0800

    Add adoption functionality

--- controllers/AdoptionsController.js
+++ controllers/AdoptionsController.js
@@ -1,3 +1,6 @@
 animalShelter.controller("AdoptionsCtrl", function AdoptionsCtrl($scope, AnimalsFactory) {
-
+ $scope.animals = AnimalsFactory.animals;
+ $scope.adoptAnimal = function(animal) {
+   animal.adopted = true;
+ };
 });
