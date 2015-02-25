ca26f7133101eba59ec93e43138735101b143bb7
Alister Fenix and Clem Freeman <clemfree@gmail.com>
2015-02-24 16:33:54 -0800

    Remove AdoptionsController

--- controllers/AdoptionsController.js
+++
@@ -1,6 +0,0 @@
-animalShelter.controller("AdoptionsCtrl", function AdoptionsCtrl($scope, AnimalsFactory) {
- $scope.animals = AnimalsFactory.animals;
- $scope.adoptAnimal = function(animal) {
-   animal.adopted = true;
- };
-});
--- controllers/AnimalsController.js
+++ controllers/AnimalsController.js
@@ -1,4 +1,9 @@
 animalShelter.controller("AnimalsCtrl", function AnimalsCtrl($scope, AnimalsFactory) {
   $scope.animals = AnimalsFactory.animals;
   $scope.AnimalsFactory = AnimalsFactory;
+
+  $scope.adoptAnimal = function(animal) {
+    animal.adopted = true;
+  };
+  
 });
